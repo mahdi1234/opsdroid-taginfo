2e14f1590b3706b9f8781c857093bff2e967c4f0
mahdi1234 <mahdi1234@centrum.cz>
2021-08-25 18:06:40 +0000

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ class TagInfo(Skill):
 		try: 
 
 			# invoke help on help keyword
-			if re.search(r'^help$', input_text):
+			if re.search(r'^help$/i', input_text):
 				await message.respond(usage)
 
 			# catch spaces
@@ -24,13 +24,13 @@ class TagInfo(Skill):
 			# querying wildcard e.g. highway=*
 			elif re.search(r'^[a-zA-Z:_]+=\*$', input_text):
 				osm_key,osm_value=input_text.split('=')
-				text = "<pre>\n### Occurence of " + osm_key + "=" + osm_value + " ###\n\n"
+				text = f"<pre>\n### Occurence of {input_text} ###\n\n"
 				# get results via taginfo API
 				with urllib.request.urlopen("https://taginfo.openstreetmap.cz/api/4/key/values?key=" + osm_key + "&filter=all&lang=cz&sortname=count&sortorder=desc&page=1&rp=10") as url:
 					data = json.loads(url.read().decode())
 					for element in data['data']:
 						# taginfo API returns empty string instead of * on website, so I had to convert it back to * and str.replace() doesn't behave as I expected thus used .re
-						text += (element['value'] + " - " + str(element['count']) + "\n")
+						text += f"{element['value']} - {str(element['count'])}\n"
 					# return everything at once as answer to chat
 					text += "</pre>"
 					await message.respond(text)
@@ -38,13 +38,13 @@ class TagInfo(Skill):
 			# querying exact combination e.g. highway=steps
 			elif re.search(r'^[a-zA-Z:_]+=[a-zA-Z:_]+$', input_text):
 				osm_key,osm_value=input_text.split('=')
-				text = "<pre>\n### Occurence of " + osm_key + "=" + osm_value + " ###\n\n"
+				text = f"<pre>\n### Occurence of {input_text} ###\n\n"
 				# get results via taginfo API
 				with urllib.request.urlopen("https://taginfo.openstreetmap.cz/api/4/tag/stats?key=" + osm_key + "&value=" + osm_value) as url:
 					data = json.loads(url.read().decode())
 					# and feed it to text string, otherwise there's problem with mixing types
 					for element in data['data']:
-						text += (element['type'] + " - " + str(element['count']) + "\n")
+						text += f"{element['type']} - {str(element['count'])}\n"
 
 				text += "\n### and most common tag combinations ###\n\n"
 				# here we go with another taginofo query
@@ -52,21 +52,20 @@ class TagInfo(Skill):
 					data = json.loads(url.read().decode())
 					for element in data['data']:
 						# taginfo API returns empty string instead of * on website, so I had to convert it back to * and str.replace() doesn't behave as I expected thus used .re
-						text += (element['other_key'] + ":" + re.sub('^$', ' *', element['other_value']) + " - " + str(element['together_count']) + "\n")
+						text += f"{element['other_key']}:{re.sub('^$', ' *', element['other_value'])} - {str(element['together_count'])}\n"
 					# return everything at once as answer to chat
 					text += "</pre>"
 					await message.respond(text)
 
 			# querying value only
 			elif re.search(r'^[a-zA-Z:_]+$', input_text):
-				osm_value=input_text
-				text = "<pre>\n### Occurence of value " + osm_value + " ###\n\n"
+				text = f"<pre>\n### Occurence of value {input_text} ###\n\n"
 				# get results via taginfo API
-				with urllib.request.urlopen("https://taginfo.openstreetmap.cz/api/4/search/by_value?query=" + osm_value + "&sortname=count_all&sortorder=desc&page=1&rp=10&format=json_pretty") as url:
+				with urllib.request.urlopen("https://taginfo.openstreetmap.cz/api/4/search/by_value?query=" + input_text + "&sortname=count_all&sortorder=desc&page=1&rp=10&format=json_pretty") as url:
 					data = json.loads(url.read().decode())
 					for element in data['data']:
 						# taginfo API returns empty string instead of * on website, so I had to convert it back to * and str.replace() doesn't behave as I expected thus used .re
-						text += (element['key'] + " = " + element['value'] + " - " + str(element['count_all']) + "\n")
+						text += f"{element['key']} = {element['value']} - {str(element['count_all'])}\n"
 					# return everything at once as answer to chat
 					text += "</pre>"
 					await message.respond(text)
